a26e2190d1846a48949bc5b272f4ef692bb076ad
FillipeEduardo <fillipe_oliveira@outlook.com.br>
2023-04-02 13:50:41 -0300

    criado os endpoints get produtos

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using storeManagerDotNet.Data;
+using storeManagerDotNet.Models;
+
+namespace storeManagerDotNet.Controllers
+{
+    [ApiController]
+    [Route("products")]
+    public class ProductController : ControllerBase
+    {
+        private readonly StoreContext _context;
+
+        public ProductController(StoreContext context) {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> Get()
+        {
+            try
+            {
+                var dados = await _context.Products.AsNoTracking().ToListAsync();
+                dados = dados.OrderBy(x => x.Id).ToList();
+
+                return Ok(dados);
+            }
+            catch (Exception ex)
+            {
+                return NotFound();
+            }
+        }
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Product>> GetById(int id)
+        {
+            try
+            {
+                var dados = await _context.Products.FindAsync(id);
+                if (dados is null)
+                {
+                    return NotFound(new { message = "Product not found" });
+                }
+                return Ok(dados);
+
+            }
+            catch (Exception ex)
+            {
+                return NotFound( new { message = "Product not found" });
+            }
+        }
+    }
+}
